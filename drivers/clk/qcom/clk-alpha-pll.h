d8487509ce2505179835c0d548ff930e8bd73b7b
ShivamKumarJha <jha.shivam3@gmail.com>
2022-04-17 14:11:00 -0500

    Add drivers/clk/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/clk/qcom/clk-alpha-pll.h
+++ drivers/clk/qcom/clk-alpha-pll.h
@@ -19,7 +19,6 @@ enum {
 	CLK_ALPHA_PLL_TYPE_TRION,
 	CLK_ALPHA_PLL_TYPE_LUCID,
 	CLK_ALPHA_PLL_TYPE_ZONDA,
-	CLK_ALPHA_PLL_TYPE_ZONDA_EVO,
 	CLK_ALPHA_PLL_TYPE_LUCID_5LPE,
 	CLK_ALPHA_PLL_TYPE_ZONDA_5LPE,
 	CLK_ALPHA_PLL_TYPE_REGERA,
@@ -185,8 +184,6 @@ extern const struct clk_ops clk_regera_pll_ops;
 extern const struct clk_ops clk_agera_pll_ops;
 
 extern const struct clk_ops clk_alpha_pll_fixed_lucid_evo_ops;
-extern const struct clk_ops clk_alpha_pll_fixed_zonda_evo_ops;
-extern const struct clk_ops clk_alpha_pll_postdiv_zonda_evo_ops;
 extern const struct clk_ops clk_alpha_pll_postdiv_lucid_evo_ops;
 extern const struct clk_ops clk_alpha_pll_lucid_evo_ops;
 
@@ -211,9 +208,6 @@ int clk_regera_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 int clk_agera_pll_configure(struct clk_alpha_pll *pll, struct regmap *regmap,
 					const struct alpha_pll_config *config);
 int clk_lucid_evo_pll_configure(struct clk_alpha_pll *pll,
-				struct regmap *regmap,
-				const struct alpha_pll_config *config);
-int clk_zonda_evo_pll_configure(struct clk_alpha_pll *pll,
 				struct regmap *regmap,
 				const struct alpha_pll_config *config);
 #endif
